7895b60ae71bbf9f9d7384b534530821816c9f4c
Morgan Fox <morganfox61297@gmail.com>
2019-05-06 14:11:30 -0400

    notes2

---
+++ practice.js
@@ -0,0 +1,6 @@
+class User {
+  constructor(email, password) {
+    this.email = email // this objects email = the email that was passed in
+    this.password = password // this objects password = the password that was passed in
+  }
+}
